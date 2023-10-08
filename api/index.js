1c87923f89750615d6d8fbd1bf47ce87dd363007
ArturoDuCas <a00833516@tec.mx>
2023-10-08 16:16:49 -0600

    feat: change client url for corse

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ const upload = multer();
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.JWT_SECRET;
 
-app.use(cors({credentials: true, origin:"http://localhost:3000"}));
+app.use(cors({credentials: true, origin:process.env.CLIENT_URL}));
 app.use(express.json());
 app.use(cookieParser());
 
